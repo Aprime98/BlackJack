d1794302d77682d37aa2de3d628782a4b035398d
CharlesMwangi <bsclmr696619@spu.ac.ke>
2023-07-27 14:35:01 +0300

    added NewCard button and randomFtn

--- index.tsx
+++ index.tsx
@@ -1,11 +1,31 @@
-let firstCard = 10;
-let secondCard = 4;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
+let cards = [firstCard, secondCard]; // Array - ordered list of items
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+
+// Create a function, getRandomCard()
+function getRandomCard() {
+  let randomNo = Math.floor(Math.random() * 13) + 1;
+  return randomNo;
+}
 
 function startGame() {
+  renderGame();
+}
+
+function renderGame() {
+  cardsEl.textContent = "Cards: ";
+
+  for (let a = 0; a < cards.length; a++) {
+    cardsEl.textContent += cards[a] + " ";
+  }
+  sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Do you want to draw a new card? 🙂";
   } else if (sum === 21) {
@@ -15,5 +35,15 @@ function startGame() {
     message = "You're out of the game! 😭";
     isAlive = false;
   }
-  console.log(message);
+
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  let newCard = getRandomCard();
+  sum += newCard;
+  // add the new card to the "cards" array
+  cards.push(newCard);
+
+  renderGame();
 }
